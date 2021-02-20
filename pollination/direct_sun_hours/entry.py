bc1416a19d9ceb0a065eb69c1873ca2bf86a02bc
Mostapha <sadeghipour@gmail.com>
2021-02-19 21:27:14 -0500

    fix: update based on changes in honeybee-radiance

--- pollination/direct_sun_hours/entry.py
+++ pollination/direct_sun_hours/entry.py
@@ -9,7 +9,7 @@ from pollination.honeybee_radiance.octree import CreateOctreeWithSky
 from pollination.alias.inputs.model import hbjson_model_input
 from pollination.alias.inputs.wea import wea_input
 from pollination.alias.inputs.north import north_input
-# from pollination.alias.outputs.daylight import sort_direct_sun_hours_results
+# from pollination.alias.outputs.daylight import sort_direct_results/direct_sun_hours
 
 from ._raytracing import DirectSunHoursEntryLoop
 
@@ -30,6 +30,11 @@ class DirectSunHoursEntryPoint(DAG):
         default=200,
         description='The maximum number of grid points per parallel execution.',
         spec={'type': 'integer', 'minimum': 1}
+    )
+
+    sensor_grid = Inputs.str(
+        description='A grid name or a pattern to filter the sensor grids. By default '
+        'all the grids in HBJSON model will be exported.', default='*'
     )
 
     model = Inputs.file(
@@ -56,21 +61,21 @@ class DirectSunHoursEntryPoint(DAG):
         ]
 
     @task(template=CreateRadianceFolder)
-    def create_rad_folder(self, input_model=model):
+    def create_rad_folder(self, input_model=model, sensor_grid=sensor_grid):
         """Translate the input model to a radiance folder."""
         return [
             {'from': CreateRadianceFolder()._outputs.model_folder, 'to': 'model'},
             {
                 'from': CreateRadianceFolder()._outputs.sensor_grids_file,
-                'to': 'results/grids_info.json'
+                'to': 'results/direct_sun_hours/grids_info.json'
             },
             {
                 'from': CreateRadianceFolder()._outputs.sensor_grids_file,
-                'to': 'cumulative-results/grids_info.json'
+                'to': 'results/cumulative/grids_info.json'
             },
             {
                 'from': CreateRadianceFolder()._outputs.sensor_grids_file,
-                'to': 'radiation-results/grids_info.json'
+                'to': 'results/direct_radiation/grids_info.json'
             },
             {
                 'from': CreateRadianceFolder()._outputs.sensor_grids,
@@ -98,7 +103,7 @@ class DirectSunHoursEntryPoint(DAG):
         return [
             {
                 'from': ParseSunUpHours()._outputs.sun_up_hours,
-                'to': 'results/sun-up-hours.txt'
+                'to': 'results/direct_sun_hours/sun-up-hours.txt'
             }
         ]
 
@@ -107,13 +112,13 @@ class DirectSunHoursEntryPoint(DAG):
         needs=[create_octree, generate_sunpath, create_rad_folder],
         loop=create_rad_folder._outputs.sensor_grids,
         sub_folder='initial_results/{{item.name}}',  # create a subfolder for each grid
-        sub_paths={'sensor_grid': 'grid/{{item.name}}.pts'}  # sub_path for sensor_grid arg
+        sub_paths={'sensor_grid': 'grid/{{item.full_id}}.pts'}  # sub_path for sensor_grid arg
     )
     def direct_sun_hours_raytracing(
         self,
         sensor_count=sensor_count,
         octree_file=create_octree._outputs.scene_file,
-        grid_name='{{item.name}}',
+        grid_name='{{item.full_id}}',
         sensor_grid=create_rad_folder._outputs.model_folder,
         sunpath=generate_sunpath._outputs.sunpath,
         sun_modifiers=generate_sunpath._outputs.sun_modifiers
@@ -122,15 +127,25 @@ class DirectSunHoursEntryPoint(DAG):
 
     results = Outputs.folder(
         source='results',
-        # alias=sort_direct_sun_hours_results
+        description='Results folder. There are 3 subfolders under results folder: '
+        'direct_sun_hours, cumulative and direct_radiation.'
     )
 
-    cumulative_results = Outputs.folder(
-        source='cumulative_results',
-        # alias=sort_direct_sun_hours_results
+    direct_sun_hours = Outputs.folder(
+        source='results/direct_sun_hours',
+        description='Hourly results for direct sun hours.',
+        # alias=sort_direct_results/direct_sun_hours
     )
 
-    radiation_results = Outputs.folder(
-        source='radiation_results',
-        # alias=sort_direct_sun_hours_results
+    cumulative_sun_hours = Outputs.folder(
+        source='results/cumulative',
+        description='Cumulative results for direct sun hours for all the input hours.',
+        # alias=sort_direct_results/direct_sun_hours
+    )
+
+    direct_radiation = Outputs.folder(
+        source='results/direct_radiation',
+        description='Hourly direct radiation results. These results only includes the '
+        'direct radiation from sun disk.',
+        # alias=sort_direct_results/direct_sun_hours
     )
